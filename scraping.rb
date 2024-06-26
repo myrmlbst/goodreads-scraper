b3aff697f879b8085a4e3f3e66077925a21876ed
Myriam <160158729+myrmlbst@users.noreply.github.com>
2024-06-26 19:05:38 +0300

    Create scraping.rb

---
+++ scraping.rb
@@ -0,0 +1,15 @@
+require 'net/http'
+require 'nokogiri'
+
+# http client
+url = 'https://www.goodreads.com/genres/classics'
+response = Net::HTTP.get_response(URI(url))
+
+if response.code != '200'
+    puts "Error: #{response.code}"
+    exit
+end
+
+# html parser
+doc = Nokogiri::HTML(response.body)
+print(doc)
